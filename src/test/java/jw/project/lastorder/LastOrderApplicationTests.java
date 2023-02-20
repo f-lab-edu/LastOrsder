d76d070a5c85825689825588281bb091bd20df10
유지웅 <poujis0@gmail.com>
2023-02-20 19:56:27 +0900

    first commit

---
+++ src/test/java/jw/project/lastorder/LastOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package jw.project.lastorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LastOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
