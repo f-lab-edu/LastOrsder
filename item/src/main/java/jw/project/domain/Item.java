19216384ae6fdf303ce49d00c3130d80c1ab4002
kbzz17 <108657069+kbzz17@users.noreply.github.com>
2023-02-22 00:54:26 +0900

    Item 추가 기능 구현 (#4)
    * build - 초기 환경 셋팅
    
    * build - [notification] 초기 환경 셋팅
    
    * build - [account] 초기 환경 셋팅
    
    * build - [shop] 초기 환경 셋팅
    
    * build - [Item] itemService 구조 및 DB 환경 구축
    
    * feat - [Item] controller addItem 기능 구현
    
    * feat - [Item] addItem 기능 Service 구현

---
+++ item/src/main/java/jw/project/domain/Item.java
@@ -0,0 +1,40 @@
+package jw.project.domain;
+
+import jakarta.persistence.*;
+import jw.project.application.command.AddItemCommand;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "tb_item")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "item_id")
+    private Long id;
+
+    private String name;
+
+    private Integer price;
+
+    private String description;
+
+    private Integer stack;
+
+    private Long storeId;
+
+    private Item(AddItemCommand command) {
+        this.name = command.getName();
+        this.price = command.getPrice();
+        this.description = command.getDescription();
+        this.stack = command.getStack();
+        this.storeId = command.getStoreId();
+    }
+
+    public static Item addItem(AddItemCommand command){
+        return new Item(command);
+    }
+}
