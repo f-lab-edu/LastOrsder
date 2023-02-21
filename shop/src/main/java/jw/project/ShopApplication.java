f62421dd7dd061b3cc8d415fc900d5f3f4c70e9c
kbzz17 <108657069+kbzz17@users.noreply.github.com>
2023-02-22 00:36:28 +0900

    초기 환경 셋팅 (#1)
    * build - 초기 환경 셋팅
    
    * build - [notification] 초기 환경 셋팅
    
    * build - [account] 초기 환경 셋팅
    
    * build - [shop] 초기 환경 셋팅

---
+++ shop/src/main/java/jw/project/ShopApplication.java
@@ -0,0 +1,13 @@
+package jw.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopApplication.class, args);
+    }
+
+}
