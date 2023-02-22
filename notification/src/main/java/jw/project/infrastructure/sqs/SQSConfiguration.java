2970fa7b289fbe70e10b9fc016ec92f33c2d18cd
kbzz17 <108657069+kbzz17@users.noreply.github.com>
2023-02-22 20:10:32 +0900

    Notification을 위한 SQS 메시지 송수신 구현 (#6)
    * build - 초기 환경 셋팅
    
    * build - [notification] 초기 환경 셋팅
    
    * build - [account] 초기 환경 셋팅
    
    * build - [shop] 초기 환경 셋팅
    
    * fix - Controller to RestController 수정
    
    * build - SQS dependency 추가
    
    * build - SQS dependency 및 AWS SQS yml 작성
    
    * feat - [Item] SQS Message 송신 구현
    
    * feat - [Notification] SQS Message 수신 구현

---
+++ notification/src/main/java/jw/project/infrastructure/sqs/SQSConfiguration.java
@@ -0,0 +1,34 @@
+package jw.project.infrastructure.sqs;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.sqs.AmazonSQSAsync;
+import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+@Slf4j
+public class SQSConfiguration {
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @Primary
+    @Bean
+    public AmazonSQSAsync amazonSQSAws() {
+        BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKey, secretKey);
+        return AmazonSQSAsyncClientBuilder.standard()
+                .withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
+                .build();
+    }
+}
